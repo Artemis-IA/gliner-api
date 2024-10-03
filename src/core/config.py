8ef6150efad561ca41f333474fe53b4333980a0d
Artemis-IA <ibrahim.mohammad@isen-ouest.yncrea.fr>
2024-10-03 12:35:33 +0200

    docker

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,25 @@
-# src/core/config.py
 from pydantic_settings import BaseSettings
 from pathlib import Path
+import os
 
 class Settings(BaseSettings):
+    # Variables d'environnement
     database_url: str
     mlflow_tracking_uri: str
+    mlflow_backend_store_uri: str
+    mlflow_artifact_root: str
+    mlflow_port: int
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_port: int
+    postgres_host: str
     prometheus_port: int = 8008
 
     class Config:
-        env_file = Path(__file__).resolve().parent.parent.parent / ".env"  # Ajuster le chemin vers .env
+        # Utilisation d'un chemin absolu pour garantir que le fichier .env est bien trouvé
+        env_file = Path(__file__).resolve().parents[2] / ".env"
+        env_file_encoding = 'utf-8'
 
+# Instanciation de la configuration
 settings = Settings()
