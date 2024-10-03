62565558f8e54d0ce8a6a0db9bb6e9aa685ba9c6
Artemis-IA <ibrahim.mohammad@isen-ouest.yncrea.fr>
2024-10-03 11:50:48 +0200

    docker

--- src/schemas/dataset.py
+++ src/schemas/dataset.py
@@ -1,16 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Dict
+from datetime import datetime
 
 class DatasetCreate(BaseModel):
-    name: str
-    data: List[Dict]  # Liste d'entités NER
+    name: str = Field(..., examples="Sample Dataset")
+    data: List[Dict] = Field(..., examples=[{"text": "Example", "entities": ["Entity1", "Entity2"]}])
 
 class DatasetUpdate(BaseModel):
-    name: str
-    data: List[Dict]
+    name: str = Field(..., examples="Updated Dataset")
+    data: List[Dict] = Field(..., examples=[{"text": "Updated Example", "entities": ["Entity3"]}])
 
 class DatasetResponse(BaseModel):
     id: int
     name: str
     data: List[Dict]
-    created_at: str
+    created_at: datetime
--- src/schemas/inference.py
+++ src/schemas/inference.py
@@ -1,11 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Dict
+from datetime import datetime
 
 class InferenceRequest(BaseModel):
-    file_path: str  # Chemin vers le fichier PDF/JPG/PNG
+    file_path: str = Field(..., examples="path/to/file.pdf")
 
 class InferenceResponse(BaseModel):
     id: int
     file_path: str
     entities: List[Dict]
-    created_at: str
+    created_at: datetime
